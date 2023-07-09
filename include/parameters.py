77e096343119af381df3407e657252fd2027f877
AngeloDamante <angelo.damante@gmail.com>
2023-07-09 14:43:23 +0200

    chore: add marker points

--- include/parameters.py
+++ include/parameters.py
@@ -7,3 +7,8 @@ intrinsic_parameters = np.load("calib_data.npz", allow_pickle=True)
 INTRINSIC_MATRIX = intrinsic_parameters['camera_matrix']
 DIST_COEFFS = intrinsic_parameters['dist_coeffs']
 
+# obj point
+MARKER_POINTS = np.array([[-MARKER_SIZE / 2, MARKER_SIZE / 2,  0],
+                          [MARKER_SIZE / 2, MARKER_SIZE / 2,  0],
+                          [MARKER_SIZE / 2, -MARKER_SIZE / 2, 0],
+                          [-MARKER_SIZE / 2, -MARKER_SIZE / 2, 0]], dtype=np.float32)
