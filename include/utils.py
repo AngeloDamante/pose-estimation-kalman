7eadb6f2b83d9a5fdbe0b1b0130df2587bc11ca6
AngeloDamante <angelo.damante@gmail.com>
2023-07-09 14:48:23 +0200

    feat: add extract_desired_corners function

--- include/utils.py
+++ include/utils.py
@@ -23,3 +23,20 @@ def remap(value: float, x_a: float, x_b: float, y_a: float, y_b: float) -> float
     value = np.clip(value, x_a, x_b)
     mapped_value = ((value - x_a) / (x_b - x_a)) * (y_b - y_a) + y_a
     return mapped_value
+
+def extract_desired_corners(id_desired, ids, corners) -> Tuple[bool, np.ndarray]:
+    """Found and extract desired marker and its corners from list of markers
+      Args:
+        id_desired (int)
+        ids (np.ndarray): Nx1
+        corners (tuple(np.ndarray)): Nx4x2
+      Returns:
+        flag (bool): true if found else otherwise
+    """
+    if len(ids) != len(corners):
+        return False, []
+    if not id_desired in ids:
+        return False, []
+
+    desired_index = np.where(ids == id_desired)[0][0]
+    return True, corners[desired_index][0]
