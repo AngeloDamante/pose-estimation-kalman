93fc282bf00a6a6947ab739b2314b287a575db7a
AngeloDamante <angelo.damante@gmail.com>
2023-07-09 14:49:14 +0200

    feat: add find_targets function

--- include/utils.py
+++ include/utils.py
@@ -24,6 +24,7 @@ def remap(value: float, x_a: float, x_b: float, y_a: float, y_b: float) -> float
     mapped_value = ((value - x_a) / (x_b - x_a)) * (y_b - y_a) + y_a
     return mapped_value
 
+
 def extract_desired_corners(id_desired, ids, corners) -> Tuple[bool, np.ndarray]:
     """Found and extract desired marker and its corners from list of markers
       Args:
@@ -40,3 +41,24 @@ def extract_desired_corners(id_desired, ids, corners) -> Tuple[bool, np.ndarray]
 
     desired_index = np.where(ids == id_desired)[0][0]
     return True, corners[desired_index][0]
+
+
+def find_targets(ids, corners, targets=TARGETS) -> Tuple[list, list]:
+    """Find Targets for input markers
+
+    Args:
+        ids (np.ndarray): Nx1
+        corners (tuple(np.ndarray)): Nx4x2
+        targets (list[int], optional):
+
+    Returns:
+        detected targets Tuple[list, list]:
+    """
+    ids_targets = []
+    corners_targets = []
+    for target in TARGETS:
+        flag, _corners = extract_desired_corners(target.value, ids, corners)
+        if flag:
+            ids_targets.append(target.value)
+            corners_targets.append(_corners)
+    return ids_targets, corners_targets
