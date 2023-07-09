81c93edc3dad8b0a1980aa8d83e13fded39415c3
AngeloDamante <angelo.damante@gmail.com>
2023-07-09 14:43:48 +0200

    chore: add working params

--- include/parameters.py
+++ include/parameters.py
@@ -1,6 +1,14 @@
 import numpy as np
 
-
+# working params
+DELTA = 0.2
+DEG_RANGE = (-4.0, 4.0)
+MARKER_SIZE = 0.084
+VISION_KF_SETTINGS = {'NX': 14,
+                      'NZ': 7,
+                      'DT': 1.0,
+                      'K1': 0.003,
+                      'K2': 1.0}
 
 # camera settings
 intrinsic_parameters = np.load("calib_data.npz", allow_pickle=True)
