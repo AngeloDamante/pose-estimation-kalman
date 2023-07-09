74625602f22118c4dba41e5a56cda13f132ce677
AngeloDamante <angelo.damante@gmail.com>
2023-07-09 15:06:13 +0200

    wip: initialization

--- main_vehicle.py
+++ main_vehicle.py
@@ -7,3 +7,13 @@ from include.definitions import configure_logger
 from src.sfm import SFM
 from src.controller import Controller
 from src.motor import Motor
+
+SPACE_BAR = 32
+
+log_level = logging.DEBUG
+logger = configure_logger()
+
+# NODES
+refgen = SFM(d_signed=DELTA, kf_enable=True)
+control = Controller(deg_range=DEG_RANGE, d_min=DELTA, d_max=2.0, speed_min=0.4, speed_max=1.0)
+motor = Motor()
